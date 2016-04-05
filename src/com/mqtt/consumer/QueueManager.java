4af43e8ce8a92445dcb565d3385a569f3c368d2a
Harold Murcia <hl.murcia222@uniandes.edu.co>
2016-04-05 02:40:58 +0000

    mejoras concurrencia

--- src/com/mqtt/consumer/QueueManager.java
+++ src/com/mqtt/consumer/QueueManager.java
@@ -12,7 +12,7 @@ import com.ibm.mq.*;
  */
 public class QueueManager {
 
-	private static String hostname = "ec2-52-87-245-160.compute-1.amazonaws.com";
+	private static String hostname = "ec2-54-89-100-47.compute-1.amazonaws.com";
 	private static String channel  = "SYSTEM.DEF.SVRCONN";
 	private static String qManager = "GRUPO8";
 	private static String queueName = "";
@@ -44,40 +44,52 @@ public class QueueManager {
 
 			MQMessage theMessage    = new MQMessage();
 			MQGetMessageOptions gmo = new MQGetMessageOptions();
-			    gmo.options=MQC.MQGMO_WAIT | MQC.MQGMO_BROWSE_FIRST + MQC.MQGMO_LOCK;
-			    gmo.matchOptions=MQC.MQMO_NONE;
-			    gmo.waitInterval=MQC.MQEI_UNLIMITED;
+			gmo.options=MQC.MQGMO_WAIT | MQC.MQGMO_BROWSE_FIRST + MQC.MQGMO_LOCK;
+			gmo.matchOptions=MQC.MQMO_NONE;
+			gmo.waitInterval=MQC.MQEI_UNLIMITED;
 
 			LocalizationManager localizacion = new LocalizationManager();
-			    
+
 			boolean thereAreMessages=true;
 			while(thereAreMessages){
-		
-				
-				if(queue.getCurrentDepth()!=0)
-				{
-					System.out.println(queue.getCurrentDepth());
-			        //read the message          
-			        queue.get(theMessage,gmo);  
-			        //print the text            
-			        String msgText = theMessage.readString(theMessage.getMessageLength());
-			        System.out.println("msg text: "+msgText);
-			        System.out.println("Fecha inicio: "+new Date().getTime());
-			        localizacion.procesarMensaje(msgText);
-			        gmo.options = MQC.MQGMO_MSG_UNDER_CURSOR;
-			        queue.get(theMessage,gmo);
-			        gmo.options = MQC.MQGMO_BROWSE_NEXT + MQC.MQGMO_LOCK + MQC.MQGMO_NO_WAIT + MQC.MQGMO_FAIL_IF_QUIESCING + MQC.MQGMO_ACCEPT_TRUNCATED_MSG; 
+
+				try{
+
+					if(queue.getCurrentDepth()!=0)
+					{
+						System.out.println(queue.getCurrentDepth());
+						//read the message          
+						queue.get(theMessage,gmo);  
+						//print the text            
+						String msgText = theMessage.readString(theMessage.getMessageLength());
+						System.out.println("msg text: "+msgText);
+						System.out.println("Fecha inicio: "+new Date().getTime());
+						localizacion.procesarMensaje(msgText);
+						gmo.options = MQC.MQGMO_MSG_UNDER_CURSOR;
+						queue.get(theMessage,gmo);
+						gmo.options = MQC.MQGMO_BROWSE_NEXT + MQC.MQGMO_LOCK + MQC.MQGMO_NO_WAIT + MQC.MQGMO_FAIL_IF_QUIESCING + MQC.MQGMO_ACCEPT_TRUNCATED_MSG; 
+					}
+
+
+				}catch (MQException e) {
+					if(e.reasonCode == e.MQRC_NO_MSG_AVAILABLE) {
+						System.out.println("no more message available or retrived");
+					}else if(e.reasonCode == e.MQRC_NO_MSG_UNDER_CURSOR){
+						System.out.println("no more message under cursor");
+					}
 				}
+
+
 			}
-			
+
 		} catch (MQException e) {
 			if(e.reasonCode == e.MQRC_NO_MSG_AVAILABLE) {
-	            System.out.println("no more message available or retrived");
-	        }else if(e.reasonCode == e.MQRC_NO_MSG_UNDER_CURSOR){
-	        	System.out.println("no more message under cursor");
-	        }
+				System.out.println("no more message available or retrived");
+			}else if(e.reasonCode == e.MQRC_NO_MSG_UNDER_CURSOR){
+				System.out.println("no more message under cursor");
+			}
 		}catch (IOException e) {
-	        System.out.println("ERROR: "+e.getMessage());
-	    }
+			System.out.println("ERROR: "+e.getMessage());
+		}
 	}
 }
